354c184adc87775c2e5be47201294e8c24cc7306
Brandon Risberg <brandon.risberg@gmail.com>
2016-04-26 20:18:23 -0700

    Minor refactor

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+var index = require('./routes/index');
 var pilots = require('./routes/pilots');
 var systems = require('./routes/systems');
 var map = require('./routes/map');
@@ -15,6 +15,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.set('view options', { pretty: app.get('env') === 'development'})
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -23,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/', index);
 app.use('/pilot', pilots);
 app.use('/system', systems);
 app.use('/map', map);
