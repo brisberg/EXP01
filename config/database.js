db5901acb5f333403f0890b93bcc4fbe24d1a476
Brandon Risberg <brandon.risberg@gmail.com>
2016-04-27 00:05:25 -0700

    Temp model / schema

--- config/database.js
+++ config/database.js
@@ -7,6 +7,28 @@ function connect(connectionString) {
     db.on('error', console.error.bind(console, 'connection error'))
     db.once('open', function callbck() {
         console.log('Mongoose connected at: ', connectionString)
+
+        var schema = mongoose.Schema({
+            name: String
+        })
+
+        var Model = mongoose.model('PilotTemplate', schema)
+
+        var pilotTemplate = new Model({name: 'Brennan'})
+        pilotTemplate.save(function(err) {
+            if (err) return console.log('error:', err.message)
+
+            console.log('it saved!:', pilotTemplate.name)
+            getPilot()
+        })
+
+        function getPilot() {
+            Model.find({name: 'Brennan'}, function(err, records) {
+                if (err) return console.log('Error retrieving pilor:', err.message)
+
+                console.log('We got found pillot', records[0].name, '!')
+            })
+        }
     })
 }
 
