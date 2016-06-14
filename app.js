3f1b4505df390de020cfffdbcd0317386f9788f2
Brandon Risberg <brandon@betterworks.com>
2016-06-13 20:46:09 -0700

    added winston and express-winston

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var winston = require('winston');
+var expressWinston = require('express-winston');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
@@ -31,13 +33,35 @@ var helper = function(path) {
   return require(appPath("app/helpers/" + path));
 };
 
+// setup logger
+if (app.get('env') === 'development') {
+  winston.level = 'debug';
+}
+winston.add(winston.transports.File, { filename: 'winston.log' });
+
+// expressWinston request logger middleware (before the router)
+app.use(expressWinston.logger({
+      transports: [
+        new winston.transports.Console({
+          json: true,
+          colorize: true
+        }),
+        new winston.transports.File({ filename: 'winston.log' })
+      ],
+      meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+      msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+      expressFormat: true, // Use the default Express/morgan request formatting, with the same colors. Enabling this will override any msg and colorStatus if true. Will only output colors on transports with colorize set to true
+      colorStatus: true, // Color the status code, using the Express/morgan color palette (default green, 3XX cyan, 4XX yellow, 5XX red). Will not be recognized if expressFormat is true
+      ignoreRoute: function (req, res) { return false; } // optional: allows to skip some log messages based on request and/or response
+    }));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'app/views'));
 app.set('view engine', 'jade');
 app.set('view options', { pretty: app.get('env') === 'development'});
 
 app.use(favicon(path.join(__dirname, 'app/public', 'favicon.ico')));
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -55,6 +79,17 @@ app.use('/pilot', route('pilots'));
 app.use('/system', route('systems'));
 app.use('/map', route('map'));
 app.use('/user', route('user'));
+
+// express winston error logger (after the router)
+app.use(expressWinston.errorLogger({
+  transports: [
+    new winston.transports.Console({
+      json: true,
+      colorize: true
+    }),
+    new winston.transports.File({ filename: 'winston.log' })
+  ]
+}));
 
 // Passport config
 require('./app/config/passport.js');
