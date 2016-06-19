b125fe692b2fc098455db8c6621707ece9c62262
Brandon Risberg <brandon@betterworks.com>
2016-06-19 13:40:58 -0700

    curl script working to upload to s3

--- dbbackup.sh
+++ dbbackup.sh
@@ -4,75 +4,15 @@
 #
 # To Do - Add logging of output.
 # To Do - Abstract bucket region to options
-
-set -e
-
-export PATH="$PATH:/usr/local/bin"
-
-usage()
-{
-cat << EOF
-usage: $0 options
-
-This script dumps the current mongo database, tars it, then sends it to an Amazon S3 bucket.
-
-OPTIONS:
-   -h      Show this message
-   -u      Mongodb user
-   -p      Mongodb password
-   -k      AWS Access Key
-   -s      AWS Secret Key
-   -r      Amazon S3 region
-   -b      Amazon S3 bucket name
-EOF
-}
+# Get the directory the script is being run from
 
 MONGODB_USER=
 MONGODB_PASSWORD=
-AWS_ACCESS_KEY=
-AWS_SECRET_KEY=
-S3_REGION=
-S3_BUCKET=
+AWS_ACCESS_KEY="<key>"
+AWS_SECRET_KEY="<secret>"
+S3_REGION="us-west-1"
+S3_BUCKET="brandon-risberg"
 
-while getopts “ht:u:p:k:s:r:b:” OPTION
-do
-  case $OPTION in
-    h)
-      usage
-      exit 1
-      ;;
-    u)
-      MONGODB_USER=$OPTARG
-      ;;
-    p)
-      MONGODB_PASSWORD=$OPTARG
-      ;;
-    k)
-      AWS_ACCESS_KEY=$OPTARG
-      ;;
-    s)
-      AWS_SECRET_KEY=$OPTARG
-      ;;
-    r)
-      S3_REGION=$OPTARG
-      ;;
-    b)
-      S3_BUCKET=$OPTARG
-      ;;
-    ?)
-      usage
-      exit
-    ;;
-  esac
-done
-
-if [[ -z $MONGODB_USER ]] || [[ -z $MONGODB_PASSWORD ]] || [[ -z $AWS_ACCESS_KEY ]] || [[ -z $AWS_SECRET_KEY ]] || [[ -z $S3_REGION ]] || [[ -z $S3_BUCKET ]]
-then
-  usage
-  exit 1
-fi
-
-# Get the directory the script is being run from
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo $DIR
 # Store the current date in YYYY-mm-DD-HHMMSS
@@ -82,13 +22,16 @@ ARCHIVE_NAME="$FILE_NAME.tar.gz"
 
 # Lock the database
 # Note there is a bug in mongo 2.2.0 where you must touch all the databases before you run mongodump
-mongo -username "$MONGODB_USER" -password "$MONGODB_PASSWORD" admin --eval "var databaseNames = db.getMongo().getDBNames(); for (var i in databaseNames) { printjson(db.getSiblingDB(databaseNames[i]).getCollectionNames()) }; printjson(db.fsyncLock());"
+# mongo -username "$MONGODB_USER" -password "$MONGODB_PASSWORD" admin --eval "var databaseNames = db.getMongo().getDBNames(); for (var i in databaseNames) { printjson(db.getSiblingDB(databaseNames[i]).getCollectionNames()) }; printjson(db.fsyncLock());"
+mongo admin --eval "printjson(db.fsyncLock());"
 
 # Dump the database
-mongodump -username "$MONGODB_USER" -password "$MONGODB_PASSWORD" --out $DIR/backup/$FILE_NAME
+# mongodump -username "$MONGODB_USER" -password "$MONGODB_PASSWORD" --out $DIR/backup/$FILE_NAME
+mongodump --db=exp01 --out $DIR/backup/$FILE_NAME
 
 # Unlock the database
-mongo -username "$MONGODB_USER" -password "$MONGODB_PASSWORD" admin --eval "printjson(db.fsyncUnlock());"
+# mongo -username "$MONGODB_USER" -password "$MONGODB_PASSWORD" admin --eval "printjson(db.fsyncUnlock());"
+mongo admin --eval "printjson(db.fsyncUnlock());"
 
 # Tar Gzip the file
 tar -C $DIR/backup/ -zcvf $DIR/backup/$ARCHIVE_NAME $FILE_NAME/
